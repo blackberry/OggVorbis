4930cbfc4132b42dfcb104b9a8b33ac0959a0f6e
Jeremy Nicholl <jnicholl@rim.com>
2011-10-29 19:52:10 -0400

    Fixed libogg build error. Previously we were generating empty libogg libraries.

--- qnx/ogg/common.mk
+++ qnx/ogg/common.mk
@@ -6,10 +6,10 @@ include $(QCONFIG)
 NAME=ogg
 
 #===== EXTRA_INCVPATH - a space-separated list of directories to search for include files.
-EXTRA_INCVPATH+=$(PRODUCT_ROOT)/include
+EXTRA_INCVPATH+=$(PRODUCT_ROOT)/../ogg/include
 
 #===== EXTRA_SRCVPATH - a space-separated list of directories to search for source files.
-EXTRA_SRCVPATH+=$(PRODUCT_ROOT)/src
+EXTRA_SRCVPATH+=$(PRODUCT_ROOT)/../ogg/src
 
 INSTALLDIR=$(firstword $(INSTALLDIR_$(OS)) usr/lib)
 	
