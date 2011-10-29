af66960d0b747ea3831aecea8ad9c39bd92a153e
Sean Paul Taylor <setaylor@rim.com>
2011-10-29 02:18:53 -0400

    Initial BlackBerry NDK 1.0 port Ogg Vorbis. This builds libvorbis (v1.3.2) and also includes source tree for libogg (v1.3.0)

---
+++ README.md
@@ -0,0 +1,1 @@
+Ogg Vorbis is a completely open, patent-free, professional audio encoding and streaming technology with all the benefits of Open Source.
---
+++ qnx/vorbis/common.mk
@@ -0,0 +1,24 @@
+ifndef QCONFIG
+QCONFIG=qconfig.mk
+endif
+include $(QCONFIG)
+
+NAME=vorbis
+
+#===== EXTRA_INCVPATH - a space-separated list of directories to search for include files.
+EXTRA_INCVPATH+=$(PRODUCT_ROOT)/../include \
+    $(PRODUCT_ROOT)/../lib \
+    $(PRODUCT_ROOT)/../lib/modes \
+    $(PRODUCT_ROOT)/../ogg/include
+
+#===== EXTRA_SRCVPATH - a space-separated list of directories to search for source files.
+EXTRA_SRCVPATH+=$(PRODUCT_ROOT)/../lib
+
+EXCLUDE_OBJS=vorbisfile.o vorbisenc.o barkmel.o tone.o
+
+INSTALLDIR=$(firstword $(INSTALLDIR_$(OS)) usr/lib)
+	
+include $(MKFILES_ROOT)/qtargets.mk
+
+OPTIMIZE_TYPE_g=none
+OPTIMIZE_TYPE=$(OPTIMIZE_TYPE_$(filter g, $(VARIANTS)))
