a932516e88f798e023fe69f5fb195ad34fc147e3
Sean Paul Taylor <setaylor@rim.com>
2011-12-19 10:25:34 -0500

    Moved [ogg] folder to be in parallel with [vorbis] folder.

---
+++ vorbis/qnx/ogg/common.mk
@@ -0,0 +1,19 @@
+ifndef QCONFIG
+QCONFIG=qconfig.mk
+endif
+include $(QCONFIG)
+
+NAME=ogg
+
+#===== EXTRA_INCVPATH - a space-separated list of directories to search for include files.
+EXTRA_INCVPATH+=$(PRODUCT_ROOT)/../../ogg/include
+
+#===== EXTRA_SRCVPATH - a space-separated list of directories to search for source files.
+EXTRA_SRCVPATH+=$(PRODUCT_ROOT)/../../ogg/src
+
+INSTALLDIR=$(firstword $(INSTALLDIR_$(OS)) usr/lib)
+	
+include $(MKFILES_ROOT)/qtargets.mk
+
+OPTIMIZE_TYPE_g=none
+OPTIMIZE_TYPE=$(OPTIMIZE_TYPE_$(filter g, $(VARIANTS)))
